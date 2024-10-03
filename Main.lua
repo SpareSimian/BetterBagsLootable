6010db2b365934bf167fdb3c9c7d21fa371d0f39
Kenneth Porter <shiva+git@sewingwitch.com>
2024-10-02 21:56:16 -0700

    Update to 110002. Update plugin style to latest BB standard with a Context.

--- Main.lua
+++ Main.lua
@@ -1,15 +1,27 @@
 local addonName, addon = ...
 LibStub('AceAddon-3.0'):NewAddon(addon, addonName, 'AceConsole-3.0')
 
+-- the prefix here is to get the category to sort to the end of the bag,
+-- since the pipe symbol's code is greater than all alphanumerics
+local categoryName = "|cffff8080Lootable|r"
+
 local bb = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
 local categories = bb:GetModule('Categories')
+local context = bb:GetModule('Context')
+local ctx = context:New('bbLootable')
 local L = bb:GetModule('Localization')
+
+-- pre-create the category so we can turn off the save flag (default)
+categories:CreateCategory(ctx, {
+   name = L:G(categoryName),
+   itemList = {}, -- required member
+   save = false,
+})
 
 categories:RegisterCategoryFunction("LootableFilter", function (data)
 
   if data.containerInfo.hasLoot then
-    -- the prefix here is to get it to sort to the end of the bag
-    return L:G("|cffff8080Lootable|r")
+    return L:G(categoryName)
   end
 
   return nil
